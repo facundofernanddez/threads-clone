a624760822f33c1423da4a97a0e05a734f95f202
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-11 23:14:29 -0300

    implement layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +27,10 @@ export default function RootLayout({
 
           <main>
             <LeftSidebar />
+
+            <section className="main-container">
+              <div className="w-full max-w-4l">{children}</div>
+            </section>
 
             <RightSidebar />
           </main>
