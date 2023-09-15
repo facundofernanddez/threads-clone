da03deb1588f0ca9ddcdfec8546beb137ca136d2
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-14 21:56:41 -0300

    Revert "create right sidebar"
    This reverts commit 6fad55f97e18fe8c49b3a69f5a1d8a63262666d7.

--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -1,14 +1,3 @@
 export default function RightSidebar() {
-  return (
-    <section className="custom-scrollbar rightsidebar">
-      <div className="flex flex-1 flex-col justify-start">
-        <h3 className="text-heading4-medium text-light-1">
-          Suggeste communities
-        </h3>
-      </div>
-      <div className="flex flex-1 flex-col justify-start">
-        <h3 className="text-heading4-medium text-light-1">Suggeste Users</h3>
-      </div>
-    </section>
-  );
+  return <div>RightSidebar</div>;
 }
