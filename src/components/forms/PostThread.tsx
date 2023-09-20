88528e2ce5e9f842ebdf7b893f0ece7e374769be
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-20 10:35:48 -0300

    create post thread component

---
+++ src/components/forms/PostThread.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { userValidation } from "@/lib/validations/user";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import * as z from "zod";
+import { Button } from "../ui/button";
+import Image from "next/image";
+import { ChangeEvent, useState } from "react";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
+
+interface AccountProfileProps {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
+export default function PostThread({ userId }: { userId: string }) {
+  const [files, setFiles] = useState<File[]>([]);
+  const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(userValidation),
+    defaultValues: {
+      profile_photo: user?.image || "",
+      name: user?.name || "",
+      username: user?.username || "",
+      bio: user?.bio || "",
+    },
+  });
+  return <h1>Post thread</h1>;
+}
