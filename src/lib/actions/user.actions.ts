a1884ce06f863e807c22961fea6ba3370e70c417
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-21 08:40:44 -0300

    change parameter and create fetch user function

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -4,14 +4,23 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
-export const updateUser = async (
-  userId: string,
-  username: string,
-  name: string,
-  bio: string,
-  image: string,
-  path: string
-): Promise<void> => {
+interface Params {
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+}
+
+export const updateUser = async ({
+  userId,
+  username,
+  name,
+  bio,
+  image,
+  path,
+}: Params): Promise<void> => {
   connectToDB();
 
   try {
@@ -34,5 +43,15 @@ export const updateUser = async (
     }
   } catch (error: any) {
     throw new Error(`Failed to create/update user: ${error.message}`);
+  }
+};
+
+export const fetchUser = async (userId: string) => {
+  try {
+    connectToDB();
+
+    return User.findOne({ id: userId });
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user: ${error.message}`);
   }
 };
