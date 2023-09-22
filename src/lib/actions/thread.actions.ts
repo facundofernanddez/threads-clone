26af93f16928e69fd899103bb39c922c9826b97a
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-22 09:46:34 -0300

    create fetch post action

--- src/lib/actions/thread.actions.ts
+++ src/lib/actions/thread.actions.ts
@@ -12,7 +12,7 @@ interface Params {
   path: string;
 }
 
-export default async function createThread({
+export async function createThread({
   text,
   author,
   communityId,
@@ -31,4 +31,34 @@ export default async function createThread({
   });
 
   revalidatePath(path);
+}
+
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children",
+      populate: {
+        path: "author",
+        model: User,
+        select: "_id name parentId image",
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
 }
