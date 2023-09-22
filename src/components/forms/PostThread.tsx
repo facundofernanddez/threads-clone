f8e613283c315ee9c95cd17d92bc325f0c260b42
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-09-21 21:43:19 -0300

    create a thread done

--- src/components/forms/PostThread.tsx
+++ src/components/forms/PostThread.tsx
@@ -6,34 +6,16 @@ import { threadValidation } from "@/lib/validations/thread";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import * as z from "zod";
 import { Button } from "../ui/button";
-import Image from "next/image";
-import { ChangeEvent, useState } from "react";
-import { isBase64Image } from "@/lib/utils";
-import { useUploadThing } from "@/lib/uploadthing";
-import { updateUser } from "@/lib/actions/user.actions";
 import { usePathname, useRouter } from "next/navigation";
-
-interface AccountProfileProps {
-  user: {
-    id: string;
-    objectId: string;
-    username: string;
-    name: string;
-    bio: string;
-    image: string;
-  };
-  btnTitle: string;
-}
+import createThread from "@/lib/actions/thread.actions";
 
 export default function PostThread({ userId }: { userId: string }) {
   const router = useRouter();
@@ -47,7 +29,16 @@ export default function PostThread({ userId }: { userId: string }) {
     },
   });
 
-  const onSubmit = () => {};
+  const onSubmit = async (values: z.infer<typeof threadValidation>) => {
+    await createThread({
+      text: values.thread,
+      author: userId,
+      communityId: null,
+      path: pathname,
+    });
+
+    router.push("/");
+  };
 
   return (
     <Form {...form}>
---
+++ src/lib/actions/thread.actions.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import Thread from "../models/thread.model";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose";
+import { revalidatePath } from "next/cache";
+
+interface Params {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+}
+
+export default async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  connectToDB();
+
+  const createdThread = await Thread.create({
+    text,
+    author,
+    community: null,
+  });
+
+  await User.findByIdAndUpdate(author, {
+    $push: { threads: createdThread._id },
+  });
+
+  revalidatePath(path);
+}
