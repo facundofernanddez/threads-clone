e66ed441bca0abf1ff8b8a2834eb7d1e223842d3
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-10-14 11:15:31 -0300

    add thread type

--- src/components/shared/ThreadsTab.tsx
+++ src/components/shared/ThreadsTab.tsx
@@ -1,6 +1,7 @@
 import { fetchUserPosts } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import ThreadCard from "../cards/ThreadCard";
+import { fetchCommunityPosts } from "@/lib/actions/community.actions";
 
 interface ThreadsTabProps {
   currentUserId: string;
@@ -13,7 +14,13 @@ export default async function ThreadsTab({
   accountId,
   accountType,
 }: ThreadsTabProps) {
-  let result = await fetchUserPosts(accountId);
+  let result: any;
+
+  if (accountType === "Community") {
+    result = await fetchCommunityPosts(accountId);
+  } else {
+    result = await fetchUserPosts(accountId);
+  }
 
   if (!result) redirect("/");
 
