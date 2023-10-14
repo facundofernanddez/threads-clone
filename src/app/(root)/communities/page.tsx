bc0f46a9c5ea66fc2b7b2bca71f47a4309c2e6d5
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-10-14 11:59:19 -0300

    create community page

--- src/app/(root)/communities/page.tsx
+++ src/app/(root)/communities/page.tsx
@@ -1,3 +1,47 @@
-export default function CommunitiesPage() {
-  return <div>Community page</div>;
+import CommunityCard from "@/components/cards/CommunityCard";
+import UserCard from "@/components/cards/UserCard";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function SearchPage() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) return redirect("/onboarding");
+
+  const result = await fetchCommunities({
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.communities.length === 0 ? (
+          <p className="no-result">No communities</p>
+        ) : (
+          <>
+            {result.communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
 }
